02b6b09a129f787d389cac62c4e46e3a60cc0d7c
Phil Burk <philburk@mobileer.com>
2026-03-25 18:00:38 -0700

    fix jsyn/examples ghost

--- src/jsyn/examples/examples.11tydata.js
+++ src/jsyn/examples/examples.11tydata.js
@@ -1,6 +1,7 @@
 module.exports = {
   extraHead: '<script src="https://cjrtnc.leaningtech.com/4.2/loader.js"></script>',
   permalink: function(data) {
-    return `/jsyn/examples/${data.page.fileSlug}.html`;
+    const stem = data.page.inputPath.split('/').pop().replace(/\.md$/, "");
+    return `/jsyn/examples/${stem}.html`;
   }
 };
